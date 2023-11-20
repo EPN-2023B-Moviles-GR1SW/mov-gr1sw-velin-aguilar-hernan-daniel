51c0bfb91162feb8db32c78263e4647b567e9969
DanielVelinrb <daniel.velin.2001@hotmail.com>
2023-11-20 12:11:17 -0500

    Hasta variables

--- 01 - Kotlin/proyecto/src/main/kotlin/main.kt
+++ 01 - Kotlin/proyecto/src/main/kotlin/main.kt
@@ -1,3 +1,42 @@
+import java.util.*
+
 fun main(){
-    println("Hola mundo");
+
+    //TIPOS VARIABLES
+    //INMUTABLES (val, no se reasignan "=")
+    val inmutable: String = "Daniel"
+
+    //MUTABLES (var, se pueden reasignar)
+    var mutable : String = "Velin"
+
+
+
+
+    //VARIALBES PRIMITIVAS
+    val nombreEstudiante : String = "Daniel Velin"
+    val sueldo : Double = 0.25
+    val estadoCivil : Char = 'S'
+    val mayorEdad : Boolean = true
+
+    //CLASES JAVA
+    val fechaNacimiento : Date = Date()
+
+
+
+
+    val estadoCivilWhen : String = "C"
+    when(estadoCivilWhen){
+        ("C") -> {
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("Viudo")
+        }
+    }
+
+    val coqueteo = if(estadoCivilWhen != "C") "SI" else "NO"
+
 }
