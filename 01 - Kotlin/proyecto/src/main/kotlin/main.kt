5cd8e22d4ee208395cb700668fd0c1b4f1f2cf2c
DanielVelin <hernan.velin@epn.edu.ec>
2023-11-26 22:25:58 -0500

    Hasta constructores

--- 01 - Kotlin/proyecto/src/main/kotlin/main.kt
+++ 01 - Kotlin/proyecto/src/main/kotlin/main.kt
@@ -40,3 +40,65 @@ fun main(){
     val coqueteo = if(estadoCivilWhen != "C") "SI" else "NO"
 
 }
+
+fun imprimirNombre(nombre : String) : Unit{
+    println("Nombre : ${nombre}")
+}
+
+//? -> Nullable
+fun calcularSueldo(sueldo : Double, tasa : Double = 12.00, bonoEspecial : Double? = null) : Double {
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }
+    else{
+        bonoEspecial.dec()
+        return sueldo * (100/tasa) + bonoEspecial
+    }
+}
+
+
+
+abstract class NumerosJava{
+    protected val numeroUno : Int
+    private val numeroDos: Int
+
+    constructor(uno : Int, dos : Int){
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializado")
+    }
+}
+
+
+abstract class Numeros(
+    //uno : Int, //Parametro
+    //Propiedades
+    protected val numeroUno : Int,
+    protected val numeroDos : Int
+){
+    init{
+        this.numeroUno; this.numeroDos
+        numeroUno; numeroDos
+        println("Inicializando")
+    }
+}
+
+
+
+class Suma(uno : Int, dos : Int) : Numeros(uno, dos){
+    init{
+        this.numeroUno;numeroUno;
+        this.numeroDos;numeroDos;
+    }
+
+    //segundo constructor
+    constructor(uno : Int? , dos : Int) : this(
+        if(uno == null) 0 else uno,
+        dos){
+        numeroDos
+        }
+
+    //tercer constructor
+    constructor(uno : Int , dos : Int?) : this(uno,
+        if(dos == null) 0 else dos)
+}
