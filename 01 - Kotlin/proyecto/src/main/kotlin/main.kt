b23ff80220c4d78d529bbdc4e8465a97c0e71950
DanielVelin <hernan.velin@epn.edu.ec>
2023-12-05 20:12:50 -0500

    Hasta trabajo en clase

--- 01 - Kotlin/proyecto/src/main/kotlin/main.kt
+++ 01 - Kotlin/proyecto/src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun main(){
 
@@ -37,8 +38,53 @@ fun main(){
         }
     }
 
-    val coqueteo = if(estadoCivilWhen != "C") "SI" else "NO"
+    //val coqueteo = if(estadoCivilWhen != "C") "SI" else "NO"
 
+    //TIPOS DE ARREGLOS
+    //ESTÁTICO
+    val arregloEstatico : Array<Int> = arrayOf(1, 2, 3)
+    println(arregloEstatico)
+    //DINÁMICO
+    val arregloDinamico : ArrayList<Int> = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    println(arregloDinamico)
+    arregloDinamico.add(10)
+    arregloDinamico.add(11)
+    println(arregloDinamico)
+
+    //OPERADORES DE ARREGLOS
+    //For Each -> Unit
+    //Iterar un arreglo
+    val respuestaForEach : Unit = arregloDinamico.forEach{
+        valorActual : Int -> println("Valor Actual : ${valorActual}")
+    }
+
+    //it -> elemento iterado
+    arregloDinamico.forEach(println(it))
+
+    arregloEstatico.forEachIndexed(){ indice : Int, valorActual ->
+        println("Valor Actual : ${valorActual} Indice : ${indice}")
+    }
+
+    //MAP --> GENERA OTRO ARREGLO
+    val respuestaMap : kotlin.collections.List<Double> = arregloDinamico.map { valorActual : Int ->
+        return@map valorActual.toDouble() + 100}
+    val respuestaMapDos = arregloDinamico.map { it + 100}
+
+    //FILTER --> RETORNA SOLO LOS ELEMENTOS QUE CUMPLEN CON LA CONDICION ESTABLECIDA
+    val respuestaFilter : kotlin.collections.List<Int> = arregloDinamico.filter { valorActual : Int ->
+        return@filter valorActual > 5}
+    val respuestaFilterDos = arregloDinamico.filter { it > 5}
+
+    //OR (Any -> Alguno cumple) ------ AND (All -> todos cumplen)
+    val respuestaAny : Boolean = arregloDinamico.any{valorActual : Int -> return@any (valorActual > 5)}
+    val respuestaAll : Boolean = arregloDinamico.all{valorActual : Int -> return@all (valorActual > 5)}
+
+    //REDUCE
+    val respuestaReduce : Int = arregloDinamico.reduce{
+        acumulado : Int,
+        valorActual -> return@reduce (acumulado + valorActual)
+    }
 }
 
 fun imprimirNombre(nombre : String) : Unit{
@@ -101,4 +147,25 @@ class Suma(uno : Int, dos : Int) : Numeros(uno, dos){
     //tercer constructor
     constructor(uno : Int , dos : Int?) : this(uno,
         if(dos == null) 0 else dos)
+
+    public fun sumar() : Int{
+        val total = numeroUno + numeroDos
+        agregarAlHistorial(total)
+        return total;
+    }
+
+    companion object{
+        val pi = 3.14;
+
+        fun elevarAlCuadrado(num : Int) : Int{
+            return num * num;
+        }
+
+        val historialSuma = arrayListOf<Int>()
+
+        fun agregarAlHistorial(valorNuevoSuma : Int){
+            historialSuma.add(valorNuevoSuma)
+        }
+    }
+
 }
--- 02 - Android/app/src/main/java/com/example/application/MainActivity.kt
+++ 02 - Android/app/src/main/java/com/example/application/MainActivity.kt
@@ -1,43 +1,24 @@
 package com.example.application
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.application.ui.theme.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+
+        setContentView(R.layout.activity_main);
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+
+        botonCicloVida.setOnClickListener{
+            irActividad(ACicloVida::class.java)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ApplicationTheme {
-        Greeting("Android")
+    fun irActividad(clase : Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
